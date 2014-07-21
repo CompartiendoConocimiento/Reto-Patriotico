6792d7a427cf9a30584e9daca9fd933262127ce7
Luis Francisco <Luisfcgm@gmail.com>
2014-07-21 13:19:09 -0500

    Reto Patriotico Dia 4

---
+++ dia4.py
@@ -0,0 +1,18 @@
+1.- para que un paquete pueda enviarse por correo 
+es necesario que la suma de su longitud y el 
+perimetro de su base no exeda de 108 pulgadas.
+Encuentre las dimenciones de la caja con base cuadrada
+de mayor volumen que se puede enviar por correo.
+
+2.- Hallar dos números positivos que minimicen la suma 
+    del doble del primero más el segundo, si el producto 
+    de los dos numeros es 288.
+
+
+encuentren los retos anteriores en 
+https://www.facebook.com/compartiendoconocimientooficial
+
+Para poder participar de este consurso, envien sus archivos ".py"
+indicando en asunto: reto patriotico
+
+al correo concurso@compartiendoconocimiento.com 
