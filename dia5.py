27fd1e45067d3753f219f00ebeec9c812daa65d4
DanteManrique <ingdantevi@gmail.com>
2014-07-22 21:20:01 +0200

    dia5
    reto patriotico dia5

---
+++ dia5.py
@@ -0,0 +1,13 @@
+En estas fiestas patrias , algunos de nosotros para poder pasar unas buenas fiestas
+patrias recurriremos a los amigos para un prestamo.
+
+Se pide crear un programa que ingresando : El monto del prestamo, el tiempo del prestamo
+(expresado en meses) y la tasa de interes mensual (expresado en porcentaje) retorne el monto
+que deberia pagar al final del prestamo y el interes simple.
+
+para poder participar del concurso enviar su archivo "*.py" al siguiente correo
+concurso@compartiendoconocimiento.com
+
+indicando en asunto RETO PATRIOTICO
+
+Importante: indicar su nombre y su cuenta de FB para podernos comunicar.
