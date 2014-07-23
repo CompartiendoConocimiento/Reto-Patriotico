61a03b79d15af195ca29c9eed024b523c530e611
DanteManrique <ingdantevi@gmail.com>
2014-07-23 22:06:11 +0200

    Reto Patriotico Dia 6
    Reto Patriotico Dia 6

---
+++ dia6.py
@@ -0,0 +1,22 @@
+Fin de semana largo !! todos de vacaciones.....!!
+y ahora a donde vamos ???
+
+teniendo como base los datos del archivo adjunto completa
+el sigueinte reto.
+
+
+
+objetivos de este reto
+
+1- Aprender a optener datos de archivos externos
+2- Creatividad e imaginacion en desarrollar la aplicacion.
+3.-Desarrollo del sentido comun
+
+teniendo como datos la cantidad de dinero que dispones, 
+los servicios que esperas, recomendacion de otros viajeros 
+y el tipo de habitacion que deseas, el sistema de debe recomendar
+a que hotel y a cual ciudad te combiene realizar tus vacaciones.
+en caso que ubiera mas de uno, el sistema te muestra esas 
+opciones
+
+el sistema no contempla a los hijos
