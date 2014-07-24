d40d0da0a11a7ef498dacfc0968af55eef9e4e9d
Luis Francisco <Luisfcgm@gmail.com>
2014-07-24 12:41:39 -0500

    DIA 7

---
+++ dia7.py
@@ -0,0 +1,23 @@
+Introducir dos numeros por teclado. Imprimir los numeros que hay entre ellos 
+comenzando por el mas pequeno. Contar cuantos hay y cuantos de ellos son 
+pares. Calcular la suma de los pares
+
+
+para poder participar del concurso enviar su archivo "*.py" al siguiente correo
+concurso@compartiendoconocimiento.com
+
+indicando en asunto RETO PATRIOTICO
+
+Importante: indicar su nombre y su cuenta de FB para podernos comunicar
+
+
+
+
+
+
+
+
+
+
+
+
